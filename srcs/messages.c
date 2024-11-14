ca3699aacc79056e9b9361be43d852fb983fd669
Jesus <ivoice666@hotmail.com>
2024-11-14 12:03:08 +0100

    inet_ntop() replaced by inet_ntoa() (not allowed in the subject)

--- srcs/messages.c
+++ srcs/messages.c
@@ -6,27 +6,19 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 18:19:03 by jesuserr          #+#    #+#             */
-/*   Updated: 2024/11/14 11:35:34 by jesuserr         ###   ########.fr       */
+/*   Updated: 2024/11/14 11:59:43 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_traceroute.h"
 
-// Although more complex than inet_ntoa(), the function inet_ntop() is better
-// choice since it is thread-safe.
-const char	*turn_ip_to_str(t_ping_data *ping_data, void *src, char *dst)
-{
-	if (inet_ntop(AF_INET, src, dst, INET_ADDRSTRLEN) == NULL)
-		print_strerror_and_exit("inet_ntop", ping_data);
-	return (dst);
-}
-
+// Function inet_ntop() is better choice since it is thread-safe. Due to subject
+// restrictions, the deprecated function inet_ntoa() has to be used.
 // traceroute to www.google.com (216.58.215.164), 64 hops max
 void	print_header(t_ping_data *ping_data)
 {
-	turn_ip_to_str(ping_data, &(ping_data->dest_addr.sin_addr), \
-	ping_data->ip_str);
-	printf("traceroute to %s (%s), ", ping_data->args.dest, ping_data->ip_str);
+	printf("traceroute to %s (%s), ", ping_data->args.dest, \
+	inet_ntoa(ping_data->dest_addr.sin_addr));
 	printf("%d hops max\n", ping_data->args.max_hops);
 }
 
@@ -60,10 +52,10 @@ void	print_consecutive_number(u_int8_t nbr)
 bool	print_response_ttl_exceeded(t_ping_data *ping_data, char *buff, \
 		struct iphdr *ip_header)
 {
-	t_icmp_packet	*inner_icmp_packet;
-	char			src_addr_str[INET_ADDRSTRLEN];
-	struct timeval	tv;
-	float			time_ms;
+	t_icmp_packet		*inner_icmp_packet;
+	struct sockaddr_in	dest_addr;
+	struct timeval		tv;
+	float				time_ms;
 
 	inner_icmp_packet = (t_icmp_packet *)(buff + (ip_header->ihl * 4) + \
 	sizeof(struct icmphdr) + sizeof(struct iphdr));
@@ -77,8 +69,8 @@ bool	print_response_ttl_exceeded(t_ping_data *ping_data, char *buff, \
 	time_ms = tv.tv_sec * 1000 + (float)tv.tv_usec / 1000;
 	if (!ping_data->printed_ip)
 	{
-		printf("%s", turn_ip_to_str(ping_data, &(ip_header->saddr), \
-		src_addr_str));
+		dest_addr.sin_addr.s_addr = ip_header->saddr;
+		printf("%s", inet_ntoa(dest_addr.sin_addr));
 		ping_data->printed_ip = true;
 	}
 	printf("  %.3fms", time_ms);
@@ -88,13 +80,13 @@ bool	print_response_ttl_exceeded(t_ping_data *ping_data, char *buff, \
 // If the packet is not addressed to us, return false to keep receiving packets.
 // If the packet is addressed to us, it means that traceroute reached its
 // destination so the flag 'destiny_reached' is set to true to break the main
-// loop and end the program.
+// loop and end the program. This is the last printed line of the program.
 bool	print_response_echo_reply(t_ping_data *ping_data, u_int16_t id, \
 		struct iphdr *ip_header)
 {
-	char			src_addr_str[INET_ADDRSTRLEN];
-	struct timeval	tv;
-	float			time_ms;
+	struct sockaddr_in	dest_addr;
+	struct timeval		tv;
+	float				time_ms;
 
 	if (id != ping_data->packet.icmp_header.un.echo.id)
 		return (false);
@@ -106,8 +98,8 @@ bool	print_response_echo_reply(t_ping_data *ping_data, u_int16_t id, \
 	time_ms = tv.tv_sec * 1000 + (float)tv.tv_usec / 1000;
 	if (!ping_data->printed_ip)
 	{
-		printf("%s", turn_ip_to_str(ping_data, &(ip_header->saddr), \
-		src_addr_str));
+		dest_addr.sin_addr.s_addr = ip_header->saddr;
+		printf("%s", inet_ntoa(dest_addr.sin_addr));
 		ping_data->printed_ip = true;
 	}
 	printf("  %.3fms", time_ms);
